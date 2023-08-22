a23d1bedb7ba8c55dbc154e9ae9ae7e4a9e5e6bb
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-08-22 12:58:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const game_open = document.querySelector('.play_button');
+const game = document.querySelector('.game_container');
+const body = document.querySelector('.body');
+const close = document.querySelector('.button_close');
+
+game_open.addEventListener('click', () => {
+  game.classList.add('game_open');
+  body.classList.add('hidden');
+});
+
+close.addEventListener('click', () => {
+  game.classList.remove('game_open');
+  body.classList.remove('hidden');
+});
